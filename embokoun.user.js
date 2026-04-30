a30c5da7adee4b90b96c0e0526642078f1aea376
hanenashi <hanenashi@gmail.com>
2026-04-30 22:42:41 +0900

    Load Vimeo service and bump version

--- embokoun.user.js
+++ embokoun.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         embokoun
 // @namespace    https://github.com/hanenashi/embokoun
-// @version      0.2.1-alpha
+// @version      0.3.0-alpha
 // @description  Modular universal embedder for okoun.cz
 // @author       hanenashi
 // @match        *://*.okoun.cz/*
@@ -20,6 +20,7 @@
 // @require      https://raw.githubusercontent.com/hanenashi/embokoun/main/src/core/blob.js
 // @require      https://raw.githubusercontent.com/hanenashi/embokoun/main/src/services/direct-mp4.js
 // @require      https://raw.githubusercontent.com/hanenashi/embokoun/main/src/services/youtube.js
+// @require      https://raw.githubusercontent.com/hanenashi/embokoun/main/src/services/vimeo.js
 // @require      https://raw.githubusercontent.com/hanenashi/embokoun/main/src/services/twitter.js
 // @require      https://raw.githubusercontent.com/hanenashi/embokoun/main/src/services/index.js
 // @require      https://raw.githubusercontent.com/hanenashi/embokoun/main/src/core/dom.js
@@ -35,7 +36,7 @@
         return;
     }
 
-    E.version = '0.2.1-alpha';
+    E.version = '0.3.0-alpha';
     E.gm.menuCommand('Embokoun settings', () => E.ui.openSettingsMenu());
 
     E.log.info('boot', 'starting', E.version);
