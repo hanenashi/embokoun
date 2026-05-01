b04f9c3affc8b5154e33e19bcf3e69c84bca6dda
hanenashi <hanenashi@gmail.com>
2026-05-01 18:46:59 +0900

    Bump namespace default to 0.4.2-alpha

--- src/core/namespace.js
+++ src/core/namespace.js
@@ -4,7 +4,7 @@
 
     const root = window.Embokoun = window.Embokoun || {};
 
-    root.version = root.version || '0.4.1-alpha';
+    root.version = root.version || '0.4.2-alpha';
     root.name = 'embokoun';
     root.githubUrl = 'https://github.com/hanenashi/embokoun';
 
