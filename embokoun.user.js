52f6dadbca7bbc1420da6f014f8ff8b1729db476
hanenashi <hanenashi@gmail.com>
2026-05-05 00:47:19 +0900

    Bump script and namespace version to 0.4.22-alpha

--- embokoun.user.js
+++ embokoun.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         embokoun
 // @namespace    https://github.com/hanenashi/embokoun
-// @version      0.4.21-alpha
+// @version      0.4.22-alpha
 // @description  Modular universal embedder for okoun.cz
 // @author       hanenashi
 // @match        *://*.okoun.cz/*
@@ -43,7 +43,7 @@
         return;
     }
 
-    E.version = '0.4.21-alpha';
+    E.version = '0.4.22-alpha';
     E.gm.menuCommand('Embokoun settings', () => E.ui.openSettingsMenu());
 
     E.log.info('boot', 'starting', E.version);
