59699c4f5a8ccea66825d6678a75c7cf3a5145f0
hanenashi <hanenashi@gmail.com>
2026-05-04 19:22:31 +0900

    Bump loader to 0.4.12-alpha

--- embokoun.user.js
+++ embokoun.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         embokoun
 // @namespace    https://github.com/hanenashi/embokoun
-// @version      0.4.11-alpha
+// @version      0.4.12-alpha
 // @description  Modular universal embedder for okoun.cz
 // @author       hanenashi
 // @match        *://*.okoun.cz/*
@@ -40,7 +40,7 @@
         return;
     }
 
-    E.version = '0.4.11-alpha';
+    E.version = '0.4.12-alpha';
     E.gm.menuCommand('Embokoun settings', () => E.ui.openSettingsMenu());
 
     E.log.info('boot', 'starting', E.version);
